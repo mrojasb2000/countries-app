e1dfd7dcfa598d87040e889f29c0fa62278191d0
Mauricio Rojas <mrojasb2000@gmail.com>
2024-04-03 17:24:14 -0300

    refactor: adding sidebar to app component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
+    AboutPageComponent,
     HomePageComponent,
-    AboutPageComponent
+    SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    AboutPageComponent,
     HomePageComponent,
-    AboutPageComponent
+    SidebarComponent
   ]
 })
 export class SharedModule { }
