9331d4e01a0f89506481419352f0a7a0ba225ff7
Mauricio Rojas <mrojasb2000@gmail.com>
2024-04-04 13:22:10 -0300

    refactor: clean component and routes unused

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    AboutPageComponent,
-    ContactPageComponent,
-    HomePageComponent,
     SidebarComponent,
   ],
   imports: [
@@ -20,9 +14,6 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     RouterModule,
   ],
   exports: [
-    AboutPageComponent,
-    ContactPageComponent,
-    HomePageComponent,
     SidebarComponent,
   ]
 })
