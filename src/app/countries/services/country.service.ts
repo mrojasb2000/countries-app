654ae80211bfa0a6c999bbf263527e6d48e11715
Mauricio Rojas <mrojasb2000@gmail.com>
2024-04-08 11:13:04 -0400

    refactor: inprove style flags

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -12,27 +12,27 @@ export class CountryService {
   constructor(private http: HttpClient) { }
 
   searchByAlphaCode( term: string): Observable<Country | null> {
-    return this.searchByTerm(term, 'alpha').pipe(
+    return this.getCountriesRequest(term, 'alpha').pipe(
       map( countries => countries.length > 0 ? countries[0]: null),
       catchError( () => of(null))
     )
   }
 
   searchByCapital( term: string): Observable<Country[]> {
-    return this.searchByTerm(term, 'capital');
+    return this.getCountriesRequest(term, 'capital');
   }
 
   searchByCountry( term: string): Observable<Country[]> {
-    return this.searchByTerm(term, 'name');
+    return this.getCountriesRequest(term, 'name');
   }
 
   searchByRegion( term: string): Observable<Country[]> {
-    return this.searchByTerm(term, 'region');
+    return this.getCountriesRequest(term, 'region');
   }
 
 
 
-  private searchByTerm(term: string, objectType: string): Observable<Country[]> {
+  private getCountriesRequest(term: string, objectType: string): Observable<Country[]> {
     const url = `${this.apiUrl}/${objectType}/${term}`;
     return this.http.get<Country[]>(url).pipe( catchError( error => of([]) ) )
   }
